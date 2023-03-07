7aa7b8e6632f9baf40a52437f10474135d39cea3
hoangtran2004 <tranhuyhoang58825@gmail.com>
2023-03-07 15:50:33 +0700

    git

---
+++ src/service/category-service.ts
@@ -0,0 +1,11 @@
+import { Category } from 'src/model/Category';
+import { AppDataSource } from './../data-source';
+class CategoryService {
+    categoryRepository: any;
+    constructor() {
+        this.categoryRepository = AppDataSource.getRepository(Category)
+    }
+    getAllCategory = async () => {
+        return await this.categoryRepository.find()
+    }
+}
